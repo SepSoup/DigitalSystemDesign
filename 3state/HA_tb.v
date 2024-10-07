98b61f891997883de9bbd485231450c6d666fd41
SepSoup <Sepehrbazmi@gmail.com>
2024-10-07 09:13:51 +0330

    16/7

---
+++ 3state/HA_tb.v
@@ -0,0 +1,21 @@
+`timescale 1 ns / 1 ps 
+module tb();
+
+reg a,b;
+wire co,s;
+
+ha ha1(a,b,co,s);
+
+integer i;
+initial 
+begin
+for (i=0;i<3;i=i+1)
+begin
+{a,b} = 2'b00; #5;
+{a,b} = 2'b01; #5;
+{a,b} = 2'b10; #5;
+{a,b} = 2'b11; #5;
+end
+
+end
+endmodule
--- HA/4-Bit_Adder_tb.v
+++ HA/4-Bit_Adder_tb.v
@@ -2,11 +2,22 @@
 
 module fba_tb();
 
-reg [3:0] a =  4'b0001;
-reg [3:0] b = 4'b0100;
+reg [3:0] a,b;
 wire [3:0] s;
 wire cout; 
 
 fourbit_adder fba1(a,b,s,cout);
+
+integer i , j;
+
+initial
+begin
+
+for(i=0;i<=15;i=i+1)
+for(j=0;j<=15;j=j+1)
+begin
+a =i; b=j; #5;
+end
+end
 
 endmodule
