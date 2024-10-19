7ae7e1f38f83f319c799b999e1b481fe6656a7e4
SepSoup <Sepehrbazmi@gmail.com>
2024-10-19 08:18:42 +0330

    fixed Behavioral Full adder

--- Behavioral Full Adder/BFA.v
+++ Behavioral Full Adder/BFA.v
@@ -1,6 +1,8 @@
 module bfa(
     input a , b , c,
-    output cout , s
+    output reg~ cout , s 
+    // we use reg because cout and s are valued based on behavoioral modeling
+    // wires are used for structual modeling.
 );
 
 
@@ -9,14 +11,14 @@ module bfa(
 always @ (a or b or c)
 begin
     case ({a,b,c})
-    3'b000 : begin cout = 0; s = 0; end;
-    3'b001 : begin cout = 0; s = 1; end;
-    3'b010 : begin cout = 0; s = 1; end;
-    3'b011 : begin cout = 1; s = 0; end;
-    3'b100 : begin cout = 0; s = 1; end;
-    3'b101 : begin cout = 1; s = 0; end;
-    3'b110 : begin cout = 1; s = 0; end;
-    3'b111 : begin cout = 1; s = 1; end;
+    3'b000 : begin cout = 0; s = 0; end
+    3'b001 : begin cout = 0; s = 1; end
+    3'b010 : begin cout = 0; s = 1; end
+    3'b011 : begin cout = 1; s = 0; end
+    3'b100 : begin cout = 0; s = 1; end
+    3'b101 : begin cout = 1; s = 0; end
+    3'b110 : begin cout = 1; s = 0; end
+    3'b111 : begin cout = 1; s = 1; end
     endcase
 
 end
